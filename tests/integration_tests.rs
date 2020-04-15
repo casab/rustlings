12abe87647c76ded10fc824dbe15b62c6aa1fd99
Engin Al <enginal@gmail.com>
2020-04-15 03:00:27 +0300

    I've done it all :p

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -122,23 +122,6 @@ fn get_hint_for_single_test() {
 }
 
 #[test]
-fn all_exercises_require_confirmation() {
-    for exercise in glob("exercises/**/*.rs").unwrap() {
-        let path = exercise.unwrap();
-        let source = {
-            let mut file = File::open(&path).unwrap();
-            let mut s = String::new();
-            file.read_to_string(&mut s).unwrap();
-            s
-        };
-        source.matches("// I AM NOT DONE").next().expect(&format!(
-            "There should be an `I AM NOT DONE` annotation in {:?}",
-            path
-        ));
-    }
-}
-
-#[test]
 fn run_compile_exercise_does_not_prompt() {
     Command::cargo_bin("rustlings")
         .unwrap()
